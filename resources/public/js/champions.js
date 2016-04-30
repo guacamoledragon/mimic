d08241953cd229760b3e92a9cf19844729732eab
Jose V. Trigueros <j.v.trigueros@gmail.com>
2016-04-30 12:19:21 -0600

    change selection whenever a champion is clicked

--- resources/public/js/champions.js
+++ resources/public/js/champions.js
@@ -1,25 +1,51 @@
 var ChampionImage = React.createClass({
-  render: function() {
+  getClassName: function () {
+    return this.props.isSelected ? 'champion-selected' : ''
+  },
+  onClick: function () {
+    this.props.updateSelection()
+  },
+  render: function () {
     return (
-      <img src={"http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/" + this.props.championName + ".png"} alt={this.props.championName}/>
+      <img
+        onClick={ this.onClick }
+        className={ this.getClassName() }
+        src={ "http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/" + this.props.championName + ".png" }
+        alt={ this.props.championName }/>
     )
   }
 })
 
 var ChampionsGrid = React.createClass({
   getInitialState: function () {
-    return { champions: [] }
+    return {
+      champions: []
+      , selectedChampion: ''
+    }
   },
   componentDidMount: function () {
-   $.get('/champions-by-name', function (result) {
-     this.setState({champions: JSON.parse(result)})
-   }.bind(this))
+    $.get('/champions-by-name', function (result) {
+      let championList = JSON.parse(result).slice(0, 10)
+
+      this.setState({champions: championList})
+    }.bind(this))
+  },
+  updateSelection: function (champion) {
+    this.setState({selectedChampion: champion})
+    return this.state
   },
   render: function () {
+    let selectedChampion = this.state.selectedChampion
+    let updateSelection = this.updateSelection
+
     return (
       <ul>
         {this.state.champions.map(function (champion) {
-          return <ChampionImage key={champion} championName={champion} />
+          return <ChampionImage
+            key={ champion }
+            isSelected={ champion === selectedChampion }
+            updateSelection={ updateSelection.bind(null, champion) }
+            championName={ champion }/>
         })}
       </ul>
     )
